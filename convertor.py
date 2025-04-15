0c726d19c1da8baea6784e9ee81a164d7540e62f
tony narloch <tnarloch@hotmail.co.uk>
2025-04-15 13:25:19 +0100

    Completed the GUI, adding Complete and Edit and Exit buttons.

--- convertor.py
+++ convertor.py
@@ -1,5 +1,6 @@
 import FreeSimpleGUI as sg
 
+
 label_1 = sg.Text("Enter feet:")
 input_1 = sg.Input()
 
@@ -7,12 +8,13 @@ label_2 = sg.Text("Enter inches:")
 input_2 = sg.Input()
 
 convert_button = sg.Button("Convert")
+output_label = sg.Text("", key="output")
 
 
 
 window = sg.Window("Convertor", layout=[[label_1, input_1],
                                         [label_2, input_2],
-                                        [convert_button]
+                                        [convert_button, output_label]
                                         ])
 window.read()
 window.close()
--- file_zipper.py
+++ file_zipper.py
@@ -2,11 +2,11 @@ import FreeSimpleGUI as sg
 
 label_1 = sg.Text("Select files to compress: ")
 input_1 = sg.Input()
-choose_button_1 = sg.FileBrowse("Choose")
+choose_button_1 = sg.FileBrowse("Choose", key = "files")
 
 label_2 = sg.Text("Select destination folder: ")
 input_2 = sg.Input()
-choose_button_2 = sg.FolderBrowse("Choose")
+choose_button_2 = sg.FolderBrowse("Choose", key = "folder")
 
 compress_button = sg.Button("Compress")
 
@@ -14,5 +14,11 @@ window = sg.Window("File Compressor",
                    layout =[[label_1, input_1, choose_button_1],
                    [label_2, input_2, choose_button_2],
                     [compress_button]])
-window.read()
+
+while True:
+    event, values = window.read()
+    print(event, values)
+    filepaths = values["files"].split(";"),
+    folder = values["folder"]
+
 window.close()
